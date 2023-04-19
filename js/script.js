3d4ff1a35086f462c9d5a8539494a5e344f6cf84
Mosarrof-Coding <tinaberry112188@gmail.com>
2023-04-19 13:04:22 +0600

    updatted

--- js/script.js
+++ js/script.js
@@ -14,6 +14,13 @@ $('.gameplaySlider').slick({
     cssEase: 'linear',
     responsive: [
       {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
         breakpoint: 992,
         settings: {
           slidesToShow: 1,
@@ -45,6 +52,13 @@ $(function(){
         breakpoint: 992,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
           slidesToScroll: 1,
         }
       },
